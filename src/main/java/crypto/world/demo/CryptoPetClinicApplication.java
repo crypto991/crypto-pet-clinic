c1731b7c066794536ef272bdc468a3e0d2ae4f4d
Djole <djordje2110@gmail.com>
2018-12-15 22:25:59 +0100

    Initial Project Add

---
+++ src/main/java/crypto/world/demo/CryptoPetClinicApplication.java
@@ -0,0 +1,14 @@
+package crypto.world.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPetClinicApplication.class, args);
+    }
+
+}
+
