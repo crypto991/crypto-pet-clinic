5f70a66ae3916cf31171039564f330496a85c595
Djole <djordje2110@gmail.com>
2018-12-16 18:22:29 +0100

    Added Owners List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/crypto/world/demo/controllers/VetController.java
+++ pet-clinic-web/src/main/java/crypto/world/demo/controllers/VetController.java
@@ -4,11 +4,13 @@ package crypto.world.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets(){
 
         return "vets/index";
