e98cd5b76461a2fc54a0db8a2c4b18023cd353e8
Djole <djordje2110@gmail.com>
2018-12-16 16:29:46 +0100

    Refactorting to common crud interface. Closes #9

--- pet-clinic-data/src/main/java/crypto/world/demo/services/PetService.java
+++ pet-clinic-data/src/main/java/crypto/world/demo/services/PetService.java
@@ -2,13 +2,7 @@ package crypto.world.demo.services;
 
 import crypto.world.demo.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById (Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
