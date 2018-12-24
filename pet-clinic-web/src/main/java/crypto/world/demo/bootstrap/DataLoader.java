f7d4cccfb66101d1be1293f4a9e21a6d43aa1ed9
Djole <djordje2110@gmail.com>
2018-12-24 19:09:38 +0100

    Added PetTypes to bootstrap dataloader.Closes #31

--- pet-clinic-web/src/main/java/crypto/world/demo/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/crypto/world/demo/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package crypto.world.demo.bootstrap;
 
 import crypto.world.demo.model.Owner;
+import crypto.world.demo.model.PetType;
 import crypto.world.demo.model.Vet;
 import crypto.world.demo.services.OwnerService;
+import crypto.world.demo.services.PetTypeService;
 import crypto.world.demo.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
