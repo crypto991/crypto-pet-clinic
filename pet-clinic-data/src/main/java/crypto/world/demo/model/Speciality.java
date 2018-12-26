8873dfd6d2f8c2917cca69a56aca71f3340c7e3e
Djole <djordje2110@gmail.com>
2018-12-26 16:47:52 +0100

    adding Project Lombok, Closes #50

--- pet-clinic-data/src/main/java/crypto/world/demo/model/Speciality.java
+++ pet-clinic-data/src/main/java/crypto/world/demo/model/Speciality.java
@@ -1,22 +1,21 @@
 package crypto.world.demo.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
